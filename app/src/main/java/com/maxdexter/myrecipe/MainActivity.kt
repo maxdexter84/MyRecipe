bd40fd9ab82a80db4d382b8c22c60a68011f7481
maxdexter <maxdexter@bk.ru>
2020-08-26 16:20:57 +0300

    добавил item в appbar  и BottomsheetFragment.kt

--- app/src/main/java/com/maxdexter/myrecipe/MainActivity.kt
+++ app/src/main/java/com/maxdexter/myrecipe/MainActivity.kt
@@ -1,14 +1,37 @@
 package com.maxdexter.myrecipe
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.maxdexter.myrecipe.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setSupportActionBar(bottom_app_bar)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.bottom_app_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.app_bar_search -> Toast.makeText(this, "clicked search", Toast.LENGTH_SHORT).show()
+            R.id.app_bar_settings -> Toast.makeText(this, "clicked settings", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
