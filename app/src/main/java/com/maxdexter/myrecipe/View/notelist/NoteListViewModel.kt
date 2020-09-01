ee92ad865beface11a9812c30d1fdcf3c2ac0fb7
maxdexter <maxdexter@bk.ru>
2020-09-01 12:47:23 +0300

    добавил зависимости room

--- app/src/main/java/com/maxdexter/myrecipe/View/notelist/NoteListViewModel.kt
+++ app/src/main/java/com/maxdexter/myrecipe/View/notelist/NoteListViewModel.kt
@@ -4,4 +4,6 @@ import androidx.lifecycle.ViewModel
 
 class NoteListViewModel : ViewModel() {
 
+    
+
 }
--- app/src/main/java/com/maxdexter/myrecipe/model/Note.kt
+++ app/src/main/java/com/maxdexter/myrecipe/model/Note.kt
@@ -1,8 +1,12 @@
 package com.maxdexter.myrecipe.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
-
+@Entity
 data class Note(
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
     val uuid: UUID,
     val typeNote: String,
